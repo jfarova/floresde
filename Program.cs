bf8bf33ae7967280c5472ddd78df53348866cd00
chenaoh <jfarova112333@gmail.com>
2022-11-20 20:50:17 -0500

    Modificar el programa anterior para que en lugar de leer 8 números, se puedan leer tantos números como se quiera hasta que llegue un cero. El resultado es la suma de todos los números leídos

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+       int num=1,suma=1;
+       while(num!=0){
+        Console.WriteLine("ingrese numero");
+        num=int.Parse(Console.ReadLine());
+         suma=suma+num;
+
+  }
+
+  Console.WriteLine("La suma de los números es "+suma);
+
+        }
+}
+
